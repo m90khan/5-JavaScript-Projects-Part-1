4d1a181628161b0d48e40810917d086f4832e1a5
Khan Mohsin <m90khan@gmail.com>
2020-08-17 20:10:37 +0500

    updating with webpack

---
+++ app/assets/scripts/APP.js
@@ -0,0 +1,8 @@
+import "../styles/main.scss";
+import "../scripts/modules/cursor";
+import "lazysizes";
+import Navigation from "../scripts/modules/navigation";
+
+if (module.hot) {
+  module.hot.accept();
+}
