367eebb47751461f0921d9696d7a815ea495f30f
Khan Mohsin <m90khan@gmail.com>
2020-11-16 22:40:08 +0500

    test case

--- app/assets/scripts/APP.js
+++ app/assets/scripts/APP.js
@@ -7,31 +7,6 @@ import '../scripts/modules/github/githubApp';
 import '../scripts/modules/movieSearch/movieSearch';
 
 import 'lazysizes';
-// import Navigation from "../scripts/modules/navigation";
-// const durationInput = document.querySelector('#duration-timer');
-// const startButton = document.querySelector('#start-timer');
-// const pauseButton = document.querySelector('#pause-timer');
-// const circle = document.querySelector('circle');
-
-// const perimeter = circle.getAttribute('r') * 2 * Math.PI;
-// circle.setAttribute('stroke-dasharray', perimeter);
-
-// let duration;
-// const timer = new Timer(durationInput, startButton, pauseButton, {
-//   onStart(totalDuration) {
-//     duration = totalDuration;
-//     console.log(circle);
-//   },
-//   onTick(timeRemaining) {
-//     circle.setAttribute(
-//       'stroke-dashoffset',
-//       (perimeter * timeRemaining) / duration - perimeter
-//     );
-//   },
-//   onComplete() {
-//     console.log('Timer is completed');
-//   },
-// });
 
 if (module.hot) {
   module.hot.accept();
